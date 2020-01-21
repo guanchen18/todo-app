f7ddd4ee2e66c614207834234a7449d2154bacfa
Guanchen <guanchen.song.797@gmail.com>
2020-01-21 14:44:49 +0100

    Add delete function by clicking a todo

--- my-todo-app/src/App.js
+++ my-todo-app/src/App.js
@@ -14,12 +14,19 @@ class App extends Component {
     let todos = [...this.state.todos, todo];
     this.setState({todos})
   }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(
+      todo => {
+        return todo.id !== id 
+      });
+      this.setState({todos})
+  }
   render () {
     console.log(this.props)
     return (
       <div>
       <h1>Todo's</h1>
-      <Todos todos={this.state.todos} />
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       <AddTodo addTodo={this.addTodo} />
     </div>
     )   
