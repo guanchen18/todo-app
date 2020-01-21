8c66f43612f3573937a6772cc60c7671a49b6396
Guanchen <guanchen.song.797@gmail.com>
2020-01-21 15:09:20 +0100

    Makeup form, todo list

--- my-todo-app/src/Todos.js
+++ my-todo-app/src/Todos.js
@@ -5,17 +5,17 @@ const Todos = ({todos, deleteTodo}) => {
     const todoList=todos.length ? (
         todos.map(todo => {
             return (
-                <div key={todo.id}>
+                <div key={todo.id} className="collection-item">
                     <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
                 </div>
             )
         })
     ) : (
-        <p>you have no todo's left, yay!</p>
+        <p className="center">you have no todo's left, yay!</p>
     )
 
     return (
-        <div>
+        <div className="Todos collection">
         {todoList}
         </div>
     )
